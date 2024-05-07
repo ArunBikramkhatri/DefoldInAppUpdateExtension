2eb484e9f79c612d0ef174a2bbc2ec75500ef119
arun_khatri <arun@callbreak.com>
2024-05-07 15:29:20 +0545

    added gms play dependencies

--- InAppUpdateExtension/myextension/src/java/com/rummy/inAppUpdate/InAppUpdateJNI.java
+++ InAppUpdateExtension/myextension/src/java/com/rummy/inAppUpdate/InAppUpdateJNI.java
@@ -6,7 +6,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-
 //import com.google.android.gms.tasks.Task;
 import com.google.android.play.core.tasks.Task;
 import com.google.android.play.core.appupdate.AppUpdateInfo;
@@ -15,7 +14,6 @@ import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
 import com.google.android.play.core.appupdate.AppUpdateOptions;
 import com.google.android.play.core.install.model.AppUpdateType;
 import com.google.android.play.core.install.model.UpdateAvailability;
-
 
 public class InAppUpdateJNI {
 
@@ -33,7 +31,6 @@ public class InAppUpdateJNI {
     checkUpdate();
   }
 
-
   void checkUpdate() {
     try {
       Log.d(TAG, "checkUpdate: 0");
@@ -41,31 +38,31 @@ public class InAppUpdateJNI {
       Log.d(TAG, "checkUpdate: 1");
       // Returns an intent object that you use to check for an update.
       Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
-// Checks that the platform will allow the specified type of update.
+      // Checks that the platform will allow the specified type of update.
 
       Log.d(TAG, "checkUpdate: 2");
       appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
         if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                // This example applies an immediate update. To apply a flexible update
-                // instead, pass in AppUpdateType.FLEXIBLE
-                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+            // This example applies an immediate update. To apply a flexible update
+            // instead, pass in AppUpdateType.FLEXIBLE
+            && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
           // Request the update.
           Log.d(TAG, "checkUpdate: available");
-          appUpdateManager.startUpdateFlow(appUpdateInfo ,
-                  activity , AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build() );
+          appUpdateManager.startUpdateFlow(appUpdateInfo,
+              activity, AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build());
         }
-        if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_NOT_AVAILABLE){
+        if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_NOT_AVAILABLE) {
           Log.d(TAG, "checkUpdate: not available");
         }
 
-        if (appUpdateInfo.updateAvailability() == UpdateAvailability.UNKNOWN){
+        if (appUpdateInfo.updateAvailability() == UpdateAvailability.UNKNOWN) {
           Log.d(TAG, "checkUpdate: unknown");
         }
       });
       Log.d(TAG, "checkUpdate: 3");
     } catch (Exception e) {
       e.printStackTrace();
-      Log.d(TAG, "checkUpdate: " +  e.getLocalizedMessage());
+      Log.d(TAG, "checkUpdate: " + e.getLocalizedMessage());
       throw new RuntimeException(e);
     }
   }
